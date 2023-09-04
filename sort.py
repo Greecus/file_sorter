56c0d3934cb2be87fc698eb2ab8e47b98e457280
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2023-09-04 03:18:09 +0200

    added: dict with supported file extensions, normalize function, folders handling,

--- sort.py
+++ sort.py
@@ -1,9 +1,8 @@
 from sys import argv
-from os.path import isfile, isdir 
-from os import listdir
+from os.path import isdir 
+from os import listdir, rmdir, mkdir
 import shutil
 import re
-
 
 
 if len(argv)>=2:
@@ -11,8 +10,23 @@ if len(argv)>=2:
 else:
     path=input('Input path to folder you wish to sort: ')
 
-def sort(path):
-    while True:    
+
+supported_extensions={
+'images':  ('JPEG', 'PNG', 'JPG', 'SVG'),
+'video':    ('AVI', 'MP4', 'MOV', 'MKV'),
+'documents':('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
+'audio':    ('MP3', 'OGG', 'WAV', 'AMR'),
+'archives': ('ZIP', 'GZ', 'TAR')
+}
+extension_to_folder_dict={}
+for key in supported_extensions:
+    if not isdir(f'{path}/{key}'):
+        mkdir(path,key)
+    for extension in supported_extensions[key]:
+        extension_to_folder_dict.update({extension.lower():key})
+
+def sort(path,top_folder):
+    while True:
         try:
             files_in_directory=listdir(path)
         except WindowsError:
@@ -20,14 +34,25 @@ def sort(path):
         else:
             break
     for file in files_in_directory:
-        print(file)
-        if isdir(f'{path}/{file}'):
-            sort(f'{path}/{file}')
+        file_path=f'{path}/{file}'
+        if isdir(file_path) and file not in ['archives','video','audio','documents','images']:
+            sort(file_path,top_folder)
+            rmdir(file_path)
         else:
-            name_and_extension=re.match('(\S+).(\S+)',file)
-            print(name_and_extension.groups())
+            file=file.rsplit('.',1)
+            file[0]=normalize(file[0])
+            try:
+                shutil.move(file_path,top_folder+extension_to_folder_dict[file[1]])
+            except:
+                pass
+
+    
 
 def normalize(string):
-    pass
+    trans_table=str.maketrans('ĄąĆćĘęŃńÓóŚśŻżŹź','AaCcEeNnOoSsZzZz')
+    normalized_string = string.translate(trans_table)
+    if not normalized_string.isalnum():
+        normalized_string=re.sub('\W+',lambda match: '_'*len(match.group(0)),normalized_string)
+    return normalized_string
 
-sort(path)
+sort(path,path)
