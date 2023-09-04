4d01298e6836674e400bbfdf7055bbe2b9859227
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2023-09-04 19:35:26 +0200

    finished added: moving and renaming files, creating text file listing all sorted files

--- sort.py
+++ sort.py
@@ -1,55 +1,65 @@
 from sys import argv
-from os.path import isdir 
-from os import listdir, rmdir, mkdir
+import os
 import shutil
 import re
+import json
 
-
+#getting path to folder to sort from console or from arguments
 if len(argv)>=2:
     path=argv[1]
 else:
     path=input('Input path to folder you wish to sort: ')
 
-
-supported_extensions={
-'images':  ('JPEG', 'PNG', 'JPG', 'SVG'),
+#creating dictionaries folder:extention and extention:folder for further use
+folders_to_extensions={
+'images':   ('JPEG', 'PNG', 'JPG', 'SVG'),
 'video':    ('AVI', 'MP4', 'MOV', 'MKV'),
 'documents':('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
 'audio':    ('MP3', 'OGG', 'WAV', 'AMR'),
 'archives': ('ZIP', 'GZ', 'TAR')
 }
 extension_to_folder_dict={}
-for key in supported_extensions:
-    if not isdir(f'{path}/{key}'):
-        mkdir(path,key)
-    for extension in supported_extensions[key]:
+for key in folders_to_extensions:
+    if not os.path.isdir(os.path.join(path,key)):
+        os.mkdir(os.path.join(path,key))
+    for extension in folders_to_extensions[key]:
         extension_to_folder_dict.update({extension.lower():key})
 
+#main part of the app. path for path of current folder. top_path for path of folder where sorting started(its for recognizing recurency)
 def sort(path,top_folder):
+    #getting correct path to existing directory
     while True:
         try:
-            files_in_directory=listdir(path)
+            files_in_directory=os.listdir(path)
         except WindowsError:
             path=input('Input valid path: ')
         else:
             break
+    #recognizing, normalizing, moving and renaming files
     for file in files_in_directory:
-        file_path=f'{path}/{file}'
-        if isdir(file_path) and file not in ['archives','video','audio','documents','images']:
+        file_path=os.path.join(path,file)
+        if os.path.isdir(file_path) and file not in ['archives','video','audio','documents','images']:
             sort(file_path,top_folder)
-            rmdir(file_path)
-        else:
+            os.rmdir(file_path)
+        elif os.path.isfile(file_path) and file not in ['sorted_files.txt']:
             file=file.rsplit('.',1)
-            file[0]=normalize(file[0])
-            try:
-                shutil.move(file_path,top_folder+extension_to_folder_dict[file[1]])
-            except:
-                pass
-
+            norm_file=normalize(file[0]),file[1]
+            if norm_file[1] in ('zip', 'gz', 'tar'):
+                shutil.unpack_archive(file_path,os.path.join(top_folder,extension_to_folder_dict[norm_file[1]]))
+            else:
+                os.rename(file_path,os.path.join(top_folder,extension_to_folder_dict[norm_file[1]])+'\\'+'.'.join(norm_file))
+    #creating file with list of all sorted files
+    if path==top_folder:
+        sorted_files={}
+        for folder_name in folders_to_extensions:
+            files_in_folder=[path.rsplit('\\')[-1] for path in os.listdir(os.path.join(top_folder,folder_name))]
+            sorted_files.update({folder_name:files_in_folder})
+        with open(os.path.join(top_folder,'sorted_files.txt'),'w') as f:
+            json.dump(sorted_files,f)
     
-
+#function normalizing. changes polish letters to english counterparts and changes all symbols other than letters, numbers and _ to _
 def normalize(string):
-    trans_table=str.maketrans('ĄąĆćĘęŃńÓóŚśŻżŹź','AaCcEeNnOoSsZzZz')
+    trans_table=str.maketrans('ĄąĆćĘęŁłŃńÓóŚśŻżŹź','AaCcEeLlNnOoSsZzZz')
     normalized_string = string.translate(trans_table)
     if not normalized_string.isalnum():
         normalized_string=re.sub('\W+',lambda match: '_'*len(match.group(0)),normalized_string)
